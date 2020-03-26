16ec81751f71b5edb9933d233dadc876fb309a72
tqre <tkuure@gmail.com>
2020-03-26 14:19:53 +0200

    All unit tests use JUnit5 now

--- src/test/java/assignment/bookstore/domain/CategoryRepositoryTest.java
+++ src/test/java/assignment/bookstore/domain/CategoryRepositoryTest.java
@@ -4,13 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class CategoryRepositoryTest {
 
--- src/test/java/assignment/bookstore/domain/UserRepositoryTest.java
+++ src/test/java/assignment/bookstore/domain/UserRepositoryTest.java
@@ -2,13 +2,10 @@ package assignment.bookstore.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepositoryTest {
 
