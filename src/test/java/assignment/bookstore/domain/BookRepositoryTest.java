daad0491fb595817e2bf67b5bc723d118df7a1de
tqre <tkuure@gmail.com>
2020-03-26 14:17:44 +0200

    JUnit 5 tests working on BookRepository

--- src/test/java/assignment/bookstore/domain/BookRepositoryTest.java
+++ src/test/java/assignment/bookstore/domain/BookRepositoryTest.java
@@ -3,14 +3,10 @@ package assignment.bookstore.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class BookRepositoryTest {
 
