476697fcc55e8ec77431740b273c7ba7bce98215
tqre <tkuure@gmail.com>
2020-03-26 09:18:10 +0200

    first assertion test working

--- src/test/java/assignment/bookstore/BookstoreApplicationTests.java
+++ src/test/java/assignment/bookstore/BookstoreApplicationTests.java
@@ -1,16 +1,24 @@
 package assignment.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import assignment.bookstore.web.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(bookController).isNotNull();
 	}
-
 }
