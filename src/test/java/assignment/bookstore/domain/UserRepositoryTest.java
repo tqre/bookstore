47b148b5c4c9318b7ce7338012d3f3166c7369db
tqre <tkuure@gmail.com>
2020-03-26 09:43:45 +0200

    Added JPA tests for all repositories

---
+++ src/test/java/assignment/bookstore/domain/UserRepositoryTest.java
@@ -0,0 +1,21 @@
+package assignment.bookstore.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository repo;
+	
+	@Test
+	void test() {
+		
+	}
+
+}
