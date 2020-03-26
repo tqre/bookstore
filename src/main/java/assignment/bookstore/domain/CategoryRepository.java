e72716035f595ae16fe7ef501ffe9cf34ef84cf5
tqre <tkuure@gmail.com>
2020-03-26 10:45:17 +0200

    Find category by name test

--- src/main/java/assignment/bookstore/domain/CategoryRepository.java
+++ src/main/java/assignment/bookstore/domain/CategoryRepository.java
@@ -1,6 +1,9 @@
 package assignment.bookstore.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+	List<Category> findByName(String name);
 }
--- src/test/java/assignment/bookstore/domain/CategoryRepositoryTest.java
+++ src/test/java/assignment/bookstore/domain/CategoryRepositoryTest.java
@@ -2,6 +2,8 @@ package assignment.bookstore.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,9 @@ public class CategoryRepositoryTest {
 
 	@Autowired
 	private CategoryRepository repo;
+	
+	//@Autowired
+	//private BookRepository bookRepo;
 	
 	@Test
 	public void createCategoryTest() {
@@ -30,6 +35,17 @@ public class CategoryRepositoryTest {
 
 	@Test
 	public void findCategoryByName() {
+		Category cat = repo.save(new Category("TestCat"));
+		List<Category> catlist = repo.findByName("TestCat");
+		assertThat(catlist.get(0).getName().equals("TestCat"));
+	}
+	
+	@Test
+	public void findBooksByCategoryByName() {
 		
+		repo.save(new Category("TestCat"));
+		//Book b1 = bookRepo.save(new Book("Title1","Auth1","isbn",1,repo.))
+		
+		//List<Book> searched = repo.findBooksByCategory("TestCat");
 	}
 }
